9b1f6cfc0b0e6db77b03d944658edc0acddf46fb
Thiago Oliveira <101339243+thiioliveira@users.noreply.github.com>
2022-06-26 00:12:40 -0300

    Interfaces

--- controllers/LivroController.java
+++ controllers/LivroController.java
@@ -2,21 +2,26 @@ package controllers;
 import models.Livro;
 import java.util.ArrayList;
 
-public class LivroController {
+import controllers.contracts.ILivroController;
+
+public class LivroController implements ILivroController {
     private String titulo;
     private String autor;
     private String genero;
 
     private static ArrayList <Livro> livros = new ArrayList<Livro>();
 
+    @Override
     public void cadastrar(Livro livro) {
         livros.add(livro);
     }
 
+    @Override
     public ArrayList<Livro> listar(){
         return livros;
     }
 
+    @Override
     public Livro buscarPorLivro(String titulo) {
         for(Livro livroLista: livros) {
             if(livroLista.getTitulo().equals(titulo)) {
@@ -26,11 +31,13 @@ public class LivroController {
         return null;
     }
     
+    @Override
     public void removerLivro(Livro Livro){
         livros.remove(Livro);
         
     }
     
+    @Override
     public void atualizarLivro(Livro livro){
         livro = Livro.replaceAll(titulo, autor, genero);;
     }
