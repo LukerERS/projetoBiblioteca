826e7777796552f695b539947912c0d1efee0a3c
Thiago Oliveira <101339243+thiioliveira@users.noreply.github.com>
2022-06-26 00:13:52 -0300

    Interfaces

--- controllers/UsuarioController.java
+++ controllers/UsuarioController.java
@@ -1,10 +1,13 @@
 package controllers;
 import java.util.ArrayList;
+
+import controllers.contracts.IUsuarioController;
 import models.Usuario;
 
-public class UsuarioController {
+public class UsuarioController implements IUsuarioController {
     private static ArrayList <Usuario> usuarios = new ArrayList<Usuario>();
 
+    @Override
     public boolean cadastrar(Usuario usuario) {
         if(buscarPorCpf(usuario.getCpf()) == null){
         usuarios.add(usuario);
@@ -13,10 +16,12 @@ public class UsuarioController {
         return false;
     }
 
+    @Override
     public ArrayList <Usuario> listar() {
         return usuarios;
     }
 
+    @Override
     public Usuario buscarPorCpf(String cpf) {
         for(Usuario usuarioLista: usuarios){
             if (usuarioLista.getCpf().equals(cpf)) {
@@ -26,6 +31,7 @@ public class UsuarioController {
         return null;
     }
 
+    @Override
     public void removerUsuario(Usuario funcionario){
         usuarios.remove(funcionario);
         
