9276e73a272950fa00b6aee0cef160c52ad66af4
Thiago Oliveira <101339243+thiioliveira@users.noreply.github.com>
2022-06-26 00:10:21 -0300

    Interfaces

--- controllers/DisponibilidadeController.java
+++ controllers/DisponibilidadeController.java
@@ -1,15 +1,18 @@
 package controllers;
 import java.util.ArrayList;
+
+import controllers.contracts.IDisponibilidadeController;
 import models.Livro;
 
-public class DisponibilidadeController {
+public class DisponibilidadeController implements IDisponibilidadeController {
     private static ArrayList <Livro> disponibilidade = new ArrayList<Livro>();
 
+    @Override
     public void cadastrarDisponibilidade(Livro indisponivel) {
         disponibilidade.add(indisponivel);
     }
 
-
+    @Override
     public Livro buscarPorLivro(String titulo) {
         for(Livro livroLista: disponibilidade) {
             if(livroLista.getTitulo().equals(titulo)) {
@@ -19,6 +22,7 @@ public class DisponibilidadeController {
         return null;
     }
 
+    @Override
     public void removerLivro(Livro Livro){
         disponibilidade.remove(Livro);
         
