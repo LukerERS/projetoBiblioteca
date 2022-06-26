77ddb0db16b583c11167fa3ed8791a5b32fbe080
aifojeruijg <101339217+aifojeruijg@users.noreply.github.com>
2022-06-26 11:26:24 -0300

    Update DevolverLivro.java

--- views/DevolverLivro.java
+++ views/DevolverLivro.java
@@ -1,6 +1,7 @@
 package views;
 import controllers.DisponibilidadeController;
 import controllers.UsuarioController;
+import controllers.LivroController;
 import models.Livro;
 import utils.Console;
 import views.contracts.IViews;
@@ -11,17 +12,19 @@ public class DevolverLivro implements IViews{
 
     @Override
     public void renderizar() {
+        
         DisponibilidadeController disponibilidadeController = new DisponibilidadeController();
+        LivroController livroController = new LivroController();
         UsuarioController usuarioController = new UsuarioController();
         Emprestimo emprestimo = new Emprestimo();
-
+        
         System.out.println("\n ---------- Devolver Livro ----------\n");
-
         String titulo = Console.readString("Digite o titulo do livro: ");
-        Livro livro = disponibilidadeController.buscarPorLivro(titulo);
-        if(livro != null){
-            livro.setStatus("Disponível");
-            disponibilidadeController.removerLivro(livro);        
+        Livro livro2 = livroController.buscarPorLivro(titulo);
+        
+        if(livro2 != null){
+            livro2.setStatus("Disponível");
+            disponibilidadeController.removerLivro(livro2);        
 
             String cpfUsuario = Console.readString("Digite o CPF do Usuário: ");
             Usuario usuario = usuarioController.buscarPorCpf(cpfUsuario);
